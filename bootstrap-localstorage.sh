a1e87bb0db08c6cd0e83143cfd118d9b166acb23
Christian R. Vozar <christian@rogueethic.com>
2018-11-12 16:30:08 -0700

    script: support for m5d. instance types

--- bootstrap-localstorage.sh
+++ bootstrap-localstorage.sh
@@ -1,24 +1,26 @@
 #!/usr/bin/env bash
 
 disks=$(ls /dev/nvme*n1 | grep nvme | awk '{print $1}')
-diskstripped=$(echo /dev/nvme*n1)
-numdisks=$(echo /dev/nvme*n1 | wc -w)
+diskstripped=$(lsblk -noheadings -ido NAME |tr '\n' ' ')
+numdisks=$(lsblk -noheadings -ido NAME |wc -w)
 echo "NVME Disks: $diskstripped"
-if [ "$numdisks" -ge 2 ]
+if [ "$numdisks" -ge 3 ]
 then
-	export DEBIAN_FRONTEND=noninteractive
-	apt-get -q -y install mdadm --no-install-recommends
+  export DEBIAN_FRONTEND=noninteractive
+  apt-get -q -y install mdadm --no-install-recommends
   mdadm --create --verbose /dev/md0 --level=0 --name=LOCAL_STORAGE --raid-devices="$numdisks" "$diskstripped"
-	sleep 15
-	wipefs -fa /dev/md0 && mkfs.ext4 /dev/md0
-	mkdir -p /mnt/disks/dev/md0
-	mount /dev/md0 /mnt/disks/dev/md0
-	echo "/dev/md0   /mnt/disks/dev/md0       ext4    defaults,nofail   0   2" >/tmp/mnt_entry
-	sh -c 'cat /tmp/mnt_entry  >> /etc/fstab'
+  sleep 15
+  wipefs -fa /dev/md0 && mkfs.ext4 /dev/md0
+  mkdir -p /mnt/disks/dev/md0
+  mount /dev/md0 /mnt/disks/dev/md0
+  echo "/dev/md0   /mnt/disks/dev/md0       ext4    defaults,nofail   0   2" >/tmp/mnt_entry
+  sh -c 'cat /tmp/mnt_entry  >> /etc/fstab'
 else
-		wipefs -fa "$disks" && mkfs.ext4 "$disks"
-		mkdir -p /mnt/disks"$disks"
-		mount "$disks" /mnt/disks"$disks"
-		echo "$disks   /mnt/disks$disks       ext4    defaults,nofail   0   2" >/tmp/mnt_entry
-		sh -c 'cat /tmp/mnt_entry  >> /etc/fstab'
+    for diskname in $diskstripped; do
+    wipefs -fa "/dev/$diskname" && mkfs.ext4 "/dev/$diskname" || true
+    mkdir -p /mnt/disks/dev/"$diskname"
+    mount "/dev/$diskname" /mnt/disks/dev/"$diskname"
+    echo "/dev/$diskname   /mnt/disks/dev/$diskname       ext4    defaults,nofail   0   2" >/tmp/mnt_entry
+    sh -c 'cat /tmp/mnt_entry  >> /etc/fstab'
+    done
 fi
